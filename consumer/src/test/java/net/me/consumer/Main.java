faadd9542a8e102d9c2e9a03c8570bc6010fa046
francesco <vigotti.francesco@gmail.com>
2019-05-04 15:52:12 +0000

    added consumer, added class to test ( which should not compile! )

---
+++ consumer/src/test/java/net/me/consumer/Main.java
@@ -0,0 +1,26 @@
+package net.me.consumer;
+
+import org.apache.commons.csv.CSVFormat;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        // transitive dependency commons-csv is visible , as expected
+        CSVFormat format = CSVFormat.DEFAULT;
+
+        // commons-codec lib has not leaked, as expected:
+
+        // cannot use Decoder class in compile scope
+        org.apache.commons.codec.Decoder decoder;
+
+        // but available in runtime scope
+        String decoderClazzName = "org.apache.commons.codec.Decoder";
+        try{
+            Class.forName(decoderClazzName);
+            System.out.println("all fine");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
